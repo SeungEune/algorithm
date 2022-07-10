a2f0f9047cd1bb18b9e5fd70f496598ac2e66d1c
SeungEune <53071208+SeungEune@users.noreply.github.com>
2022-07-10 22:53:03 +0900

    백준_단계별로풀어보기_최소, 최대_10818
    백준_단계별로풀어보기_최소, 최대_10818

---
+++ baekJoon/java_language/src/StepByStep/Level_4/Java_10818.java
@@ -0,0 +1,49 @@
+//최소, 최대
+//문제
+//        N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+//
+//        입력
+//        첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+//
+//        출력
+//        첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+//
+//        예제 입력 1
+//        5
+//        20 10 35 30 7
+//        예제 출력 1
+//        7 35
+
+package StepByStep.Level_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.StringTokenizer;
+
+/**
+ * 풀면서 느낀점 : 백준도 stream이 된다!!
+ */
+public class Java_10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        OptionalInt minA = Arrays.stream(arr).min();
+        OptionalInt maxA = Arrays.stream(arr).max();
+
+        System.out.println(minA.getAsInt() + " " + maxA.getAsInt());
+    }
+}
